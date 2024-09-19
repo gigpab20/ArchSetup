31f6901e0519e22400e56edd71f6d15593f1eb84
gigpab20 <gigpab20@htl-kaindorf.at>
2024-09-19 11:42:15 +0200

    added bleutooth audio package

--- arch_install.sh
+++ arch_install.sh
@@ -5,6 +5,7 @@ packages=(
 	net-tools
 	bluez
 	pulseaudio
+	pulseaudio-bluetooth
 	fuse2
 	brightnessctl
 	cups
@@ -55,15 +56,6 @@ packages=(
 	wofi
 	mako
 )
-
-# set some colors
-CNT="[\e[1;36mNOTE\e[0m]"
-COK="[\e[1;32mOK\e[0m]"
-CER="[\e[1;31mERROR\e[0m]"
-CAT="[\e[1;37mATTENTION\e[0m]"
-CWR="[\e[1;35mWARNING\e[0m]"
-CAC="[\e[1;33mACTION\e[0m]"
-INSTLOG="install.log"
 
 install_software() {
 
@@ -143,7 +135,5 @@ rm -rf ~/.config/waybar/
 cp -R dotfiles/hypr ~/.config/
 cp -R dotfiles/kitty ~/.config/
 cp -R dotfiles/waybar ~/.config/
-
-
 
 reboot
