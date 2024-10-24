4e0ec8397cbbba0f752852ac077b57ba12daac20
gigpab20 <gigpab20@htl-kaindorf.at>
2024-10-24 16:11:20 +0200

    added functioning if for mounting data and install profile

--- arch_install.sh
+++ arch_install.sh
@@ -135,8 +135,11 @@ if [ ! -f /sbin/yay ]; then
 	fi
 fi
 
+echo "Hyprland or i3? (h|i)"
+read desicion
 
-#if [[$desicion == "h"]]; then
+if [ $desicion = "h" ]
+then
 
     echo -e "Installing Hyprland, this may take a while..."
     install_software hyprland
@@ -147,22 +150,39 @@ fi
     rm -rf ~/.config/hypr/
     rm -rf ~/.config/kitty/
     rm -rf ~/.config/waybar/
+    rm -rf ~/.config/mako/
+    rm -rf ~/.config/fastfetch/
 
-    cp -r dotfiles/hypr ~/.config/
-    cp -r dotfiles/kitty ~/.config/
-    cp -r dotfiles/waybar ~/.config/
 
-#else
-#    echo -e "Installing i3, this may take a while..."
-#    install_software i3-wm
-#    for SOFTWR in ${i3[@]}; do
-#	    install_software $SOFTWR
-#    done
-#
-#    rm -rf ~/.config/kitty/
-#    cp -r dotfiles/kitty ~/.config/
-#	cp -r dotfiles/dolphinrc ~/.config/
-#fi
+    cp -r dotfiles/hypr/ ~/.config/
+    cp -r dotfiles/fastfetch/ ~/.config/
+    cp -r dotfiles/kitty/ ~/.config/
+    cp -r dotfiles/waybar/ ~/.config/
+    cp -r dotfiles/mako/ ~/.config/
+
+
+else
+    echo -e "Installing i3, this may take a while..."
+    install_software i3-wm
+    for SOFTWR in ${i3[@]}; do
+	    install_software $SOFTWR
+    done
+
+	rm -rf ~/.config/i3/
+    rm -rf ~/.config/kitty/
+    rm -rf ~/.config/polybar/
+    rm -rf ~/.config/mako/
+    rm -rf ~/.config/picom/
+
+
+    cp -r dotfiles/i3/ ~/.config/
+    cp -r dotfiles/kitty/ ~/.config/
+    cp -r dotfiles/polybar/ ~/.config/
+    cp -r dotfiles/mako/ ~/.config/
+    cp -r dotfiles/picom/ ~/.config/
+
+fi
+
 
 echo -e "Installing main components, this may take a while..."
 for SOFTWR in ${packages[@]}; do
@@ -193,15 +213,18 @@ sudo ./Anaconda3-2024.06-1-Linux-x86_64.sh
 mkdir ~/Documents/
 mkdir ~/Pictures/Screenshots
 
-sudo mkdir /mnt/data_disk
-sudo mount /dev/nvme1n1p1 /mnt/data_disk
-sudo chown -R $USER:$USER /mnt/data_disk
+echo "want to mount datadrive? (y|n)"
+read desicion
+if [ $desicion = "y"]
+then
+	sudo mkdir /mnt/data_disk
+	sudo mount /dev/nvme1n1p1 /mnt/data_disk
+	sudo chown -R $USER:$USER /mnt/data_disk
 
-sudo blkid /dev/nvme1n1p1
-sudo nano /etc/fstab
-UUID=UUID=e6fed6a2-1419-452d-b970-873adbbedc3e /mnt/Data ext4 defaults 0 2 /mnt/data ext4 defaults 0 2
-
-
+	sudo blkid /dev/nvme1n1p1
+	sudo nano /etc/fstab
+	UUID=UUID=e6fed6a2-1419-452d-b970-873adbbedc3e /mnt/Data ext4 defaults 0 2 /mnt/data ext4 defaults 0 2
+fi
 
 sudo su - postgres
 initdb -D /var/lib/postgres/data
